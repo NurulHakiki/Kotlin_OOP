ff595aec1d44c6a6b5d225d0641259b80e310cce
Nurul Hakiki <nurulhakiki689@gmail.com>
2022-03-31 09:27:10 +0700

    Ini merupakan program Kotlin OOP Greeter

---
+++ src/main/kotlin/id/ac/polbeng/nurulhakiki/test_kelas/1_Greeter.kt
@@ -0,0 +1,24 @@
+package id.ac.polbeng.nurulhakiki.test_kelas
+/*
+Membuat package baru pada folder src dengan nama
+"id.ac.polbeng.nurul.test_kelas"
+ */
+class `1_Greeter` {
+    fun greet() {
+        println("Hello object world!")
+    }
+}
+
+/*
+Kelas dapat diumpamakan seperti spesifikasi atau blueprint (cetakan)
+untuk membuat sebuah objek. Anggota kelas (class member) terdiri dari,
+konstruktor dan initializer block, fungsi, property, nested dan inner
+class, dan deklarasi objek. Kelas di Kotlin sedikit berbeda
+implementasinya dengan Java, Kotlin secara default mendeklarasikan
+anggota kelas dengan akses modifier final dan visibility modifier public.
+ Deklarasi sebuah kelas terdiri dari
+  1) keyword class;
+   2) sebuah identifier sebagai nama kelas;
+    3) opsional header;
+     4) dan opsional body.
+ */
