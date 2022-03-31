677ca66686e496953704e99ce35268b65e3dbb16
Nurul Hakiki <nurulhakiki689@gmail.com>
2022-03-31 09:30:10 +0700

    Ini merupakan program Kotlin OOP GreeterTest

---
+++ src/main/kotlin/id/ac/polbeng/nurulhakiki/test_kelas/2_GreeterTest.kt
@@ -0,0 +1,11 @@
+package id.ac.polbeng.nurulhakiki.test_kelas
+
+fun main() {
+    val greeter = `1_Greeter`()
+    greeter.greet()
+}
+
+/*
+pada file GreeterTest digunkan untuk meruning class Greeter yang telah dibuat
+sebelumnya. karna pada kode diatas menyebutkan class yang telah dibuat sebelumnya.
+ */
--- src/main/kotlin/id/ac/polbeng/nurulhakiki/test_kelas/Greeter.kt
+++
@@ -1,4 +0,0 @@
-package id.ac.polbeng.nurulhakiki.test_kelas
-
-class Greeter {
-}
