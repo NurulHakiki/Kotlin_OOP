b4a76094cafbd6d1138b18c98c1896c72d15b10e
Nurul Hakiki <nurulhakiki689@gmail.com>
2022-03-31 10:17:32 +0700

    Ini merupakan program Kotlin OOP Data Class A

---
+++ src/main/kotlin/id/ac/polbeng/nurulhakiki/test_kelas/34_DataClassA.kt
@@ -0,0 +1,23 @@
+package id.ac.polbeng.nurulhakiki.test_kelas
+
+data class Student(val name: String, val age: Int)
+
+fun main() {
+    val boni = Student("Boni", 21)
+    val meri = Student("Meri", 20)
+    println("Student Name is: ${boni.name}")
+    println("Student Age is:  ${boni.age}")
+    println("Student Name is: ${meri.name}")
+    println("Student Age is:  ${meri.age}")
+}
+
+/*
+Sebuah data kelas sama seperti kelas biasa tapi memiliki tambahan
+funsionalitas. Di Kotlin kita tidak perlu menulis atau men-generate
+semua data kelas seperti method Getter dan Setter di Java, Kotlin telah
+men-generate secara otomatis fungsi Getter dan Setter pada setiap
+anggota kelas.
+ia juga menurunkan implementasi fungsi standar seperti equals(),
+hashCode() dan toString() dari property yang dideklarasikan didalam
+data kelas kontruktor utama
+ */
