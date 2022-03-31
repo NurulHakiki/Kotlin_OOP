b48dc87762476f690d7f8a11fd0835a02ab42672
Nurul Hakiki <nurulhakiki689@gmail.com>
2022-03-31 10:03:33 +0700

    Ini merupakan program Kotlin OOP PersonD

---
+++ src/main/kotlin/id/ac/polbeng/nurulhakiki/test_kelas/8_PersonD.kt
@@ -0,0 +1,28 @@
+package id.ac.polbeng.nurulhakiki.test_kelas
+
+//Secondary Construktor
+class PersonD {
+    var firstName: String
+    var lastName: String
+    var age: Int
+    constructor(_firstName: String, _lastName: String, _age: Int){
+        firstName = _firstName
+        lastName = _lastName
+        age = _age
+    }
+}
+
+fun main(){
+    val budi = PersonD("Budi", "Gunawan", 21)
+    println("Name : ${budi.firstName} ${budi.lastName}")
+    println("Age : ${budi.age}")
+}
+/*
+Konstruktor tambahan (secondary constructor) didefenisikan didalam tubuh
+kelas. Jika property kelas tidak diinisialisasi pada saat
+mendeklarasikannya maka property tersebut harus di inisialisasi didalam
+konstruktor tambahan atau blok inisialisasi. Konstruktor tambahan
+memerlukan keyword constructor untuk mendefenisikannya. Selain itu
+konstruktor tambahan boleh tidak memiliki tubuh kelas dalam
+pendefenisiannya.
+ */
