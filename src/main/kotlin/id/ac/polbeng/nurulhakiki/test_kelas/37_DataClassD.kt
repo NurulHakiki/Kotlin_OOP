e64343372b92405d2f16f7c3a034908e451b6856
Nurul Hakiki <nurulhakiki689@gmail.com>
2022-03-31 10:19:00 +0700

    Ini merupakan program Kotlin OOP Data Class D

---
+++ src/main/kotlin/id/ac/polbeng/nurulhakiki/test_kelas/37_DataClassD.kt
@@ -0,0 +1,10 @@
+package id.ac.polbeng.nurulhakiki.test_kelas
+
+fun main(){
+    val meri = Student("Meri", 20)
+    println(meri.toString())
+}
+
+/*
+Fungsi toString() pada data kelas mengembalikan nilai kembalian String yang merupakan representasi dari objek
+ */
